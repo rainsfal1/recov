5d2dbeda3e35004c46502d3e0b1b864d5db2a997
rainsfal1 <demoplayer1232@gmail.com>
2024-05-19 01:10:08 +0500

    36th attempt

--- server/server.js
+++ server/server.js
@@ -42,12 +42,12 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'index.html'));
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;

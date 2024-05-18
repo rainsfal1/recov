0730b13b82fe45da2c6bc2ceaed32bc451f85b33
rainsfal1 <demoplayer1232@gmail.com>
2024-05-18 05:14:09 +0500

    tweenteeth attempt

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname, './')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 const PORT = process.env.PORT || 3000;

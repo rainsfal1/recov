4be52bb8a475b5b8a5d0e35c8a58d07f1e51c57a
rainsfal1 <demoplayer1232@gmail.com>
2024-05-19 20:11:31 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	server/server.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: path.resolve(__dirname, './'),
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, './src/index.tsx'),
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  publicDir: path.resolve(__dirname, './public'),
 });

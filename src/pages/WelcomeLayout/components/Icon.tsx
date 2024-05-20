c72195b8b389a4a73615fda57dadca99c1b9ff3d
rainsfal1 <demoplayer1232@gmail.com>
2024-05-21 02:01:05 +0500

    OK

---
+++ src/pages/WelcomeLayout/components/Icon.tsx
@@ -0,0 +1,111 @@
+import "../welcomelayout.css";
+
+// Icon component
+export default function Icon() {
+  return (
+    <div className="main-icon">
+      <svg
+        className="main-icon-svg"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        width="512"
+        height="512"
+        viewBox="0 0 64 64"
+        style={{ backgroundColor: "new 0 0 512 512" }}
+      >
+        <g>
+          <path
+            fill="#85beff"
+            d="M7 35h50l5 26H2z"
+            opacity="1"
+            data-original="#85beff"
+          ></path>
+          <path
+            fill="#71aaff"
+            d="M4.037 61H62l-4.615-24H8.652z"
+            opacity="1"
+            data-original="#71aaff"
+          ></path>
+          <path
+            fill="#f5ffff"
+            d="m60.077 51-1.154-6H47V35h-6v10h-6V35h-6v10H19V35h-6v10H5.077l-1.154 6H17v10h6V51h12v10h6V51z"
+            opacity="1"
+            data-original="#f5ffff"
+            className=""
+          ></path>
+          <path
+            fill="#e1fcfc"
+            d="M23 61V51h12v10h6V51h19.077l-1.154-6H47v-8h-6v8h-6v-8h-6v8H19v-8h-6v8H7.114L5.96 51H17v10z"
+            opacity="1"
+            data-original="#e1fcfc"
+            className=""
+          ></path>
+          <path
+            fill="#a8de50"
+            d="M23 51h12v10H23z"
+            opacity="1"
+            data-original="#a8de50"
+          ></path>
+          <path
+            fill="#c1e782"
+            d="M58.923 45 57 35H47v10z"
+            opacity="1"
+            data-original="#c1e782"
+          ></path>
+          <path
+            fill="#a8de50"
+            d="M57.385 37H47v8h11.923z"
+            opacity="1"
+            data-original="#a8de50"
+          ></path>
+          <path
+            fill="#ff826d"
+            d="M32 3a16.926 16.926 0 0 0-13.991 26.451L32 50l13.991-20.549A16.926 16.926 0 0 0 32 3z"
+            opacity="1"
+            data-original="#ff826d"
+            className=""
+          ></path>
+          <path
+            fill="#ff5555"
+            d="M20.009 29.451 33 48.531l12.991-19.08a16.951 16.951 0 0 0-5.835-24.387A17.975 17.975 0 0 0 34 4C20.407 4 12.359 18.215 20.009 29.451z"
+            opacity="1"
+            data-original="#ff5555"
+            className=""
+          ></path>
+          <circle
+            cx="32"
+            cy="20"
+            r="12"
+            fill="#e1fcfc"
+            opacity="1"
+            data-original="#e1fcfc"
+            className=""
+          ></circle>
+          <path
+            fill="#f5ffff"
+            d="M34 10a11.988 11.988 0 0 0-9.419 19.419 11.988 11.988 0 0 0 16.838-16.838A11.93 11.93 0 0 0 34 10z"
+            opacity="1"
+            data-original="#f5ffff"
+            className=""
+          ></path>
+          <g fill="#76736f">
+            <circle
+              cx="32"
+              cy="28"
+              r="1"
+              fill="#76736f"
+              opacity="1"
+              data-original="#76736f"
+            ></circle>
+            <path
+              d="M32 25a1 1 0 0 1-1-1v-1.753a3.986 3.986 0 0 1 2.318-3.547A3 3 0 1 0 29 16a1 1 0 0 1-2 0 5 5 0 1 1 7.2 4.492 2 2 0 0 0-1.2 1.755V24a1 1 0 0 1-1 1z"
+              fill="#76736f"
+              opacity="1"
+              data-original="#76736f"
+            ></path>
+          </g>
+        </g>
+      </svg>
+    </div>
+  );
+}
---
+++ src/pages/WelcomeLayout/components/LoginMessage.tsx
@@ -0,0 +1,9 @@
+// LoginMessage component
+export default function LoginMessage() {
+    return (
+        <div>
+            <h1 className="login-textbox-heading">Lost something?</h1>
+            <p className="login-textbox-paragraph">Let's work together to find it</p>
+        </div>
+    )
+}

61abd02b626cc05f7389287f38537ea52c8df25a
rainsfal1 <demoplayer1232@gmail.com>
2024-05-20 12:38:39 +0500

    check for page update

--- src/pages/SignIn/components/Logo.js
+++ src/pages/SignIn/components/Logo.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { PageIcon } from '../../../../public/pageIcon/pageIcon';
 const Logo = () => {
-    return (_jsxs("div", { className: "mt-48 flex items-center justify-center", children: [_jsx(PageIcon, {}), _jsx("h2", { className: "ml-8 text-6xl font-bold", children: "Welcome ba32ck!" })] }));
+    return (_jsxs("div", { className: "mt-48 flex items-center justify-center", children: [_jsx(PageIcon, {}), _jsx("h2", { className: "ml-8 text-6xl font-bold", children: "Welcome ba22ck!" })] }));
 };
 export default Logo;
